5b9fc699d22a0691cd99d55433c908cd6bddae45
Edson Yanaga <edson@yanaga.com.br>
2012-09-18 11:53:16 -0300

    Adicionada configuracao local do mysql.

---
+++ src/main/java/br/com/produtec/config/database/DesenvolvimentoDatabaseConfig.java
@@ -0,0 +1,44 @@
+package br.com.produtec.config.database;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+import br.com.produtec.config.annotation.Desenvolvimento;
+
+@Desenvolvimento
+@Configuration
+public class DesenvolvimentoDatabaseConfig implements DatabaseConfig {
+
+	@Bean
+	@Override
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+		factoryBean.setPersistenceUnitName("mysql");
+		factoryBean.setDataSource(dataSource());
+		return factoryBean;
+	}
+
+	@Bean
+	@Override
+	public DataSource dataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/produtec");
+		dataSource.setUsername("produtec");
+		dataSource.setPassword("produtec");
+		dataSource.setTestOnBorrow(true);
+		dataSource.setTestOnReturn(true);
+		dataSource.setTestWhileIdle(true);
+		dataSource.setTimeBetweenEvictionRunsMillis(1800000);
+		dataSource.setNumTestsPerEvictionRun(3);
+		dataSource.setMinEvictableIdleTimeMillis(1800000);
+		return dataSource;
+	}
+
+}
