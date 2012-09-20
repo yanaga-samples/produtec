9044d894657217d95fd0ab78f30bbf78860b1967
Edson Yanaga <edson@yanaga.com.br>
2012-09-20 10:56:12 -0300

    Corrigidos ITs.

--- src/main/java/br/com/produtec/config/root/ProdutecConfig.java
+++ src/main/java/br/com/produtec/config/root/ProdutecConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Controller;
 
 @Configuration
-@ComponentScan(basePackages = "br.com.produtec", scopedProxy = ScopedProxyMode.TARGET_CLASS, excludeFilters = @Filter(Controller.class))
+@ComponentScan(basePackages = {"br.com.produtec.app", "br.com.produtec.config.root"}, scopedProxy = ScopedProxyMode.TARGET_CLASS, excludeFilters = @Filter(Controller.class))
 public class ProdutecConfig {
 
 }
