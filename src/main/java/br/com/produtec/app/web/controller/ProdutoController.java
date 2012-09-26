015f731672e21517a8129f509db98ad16f59f7cb
Edson Yanaga <edson@yanaga.com.br>
2012-09-26 10:10:27 -0300

    Corrigido import de QProduto.

--- src/main/java/br/com/produtec/app/web/controller/ProdutoController.java
+++ src/main/java/br/com/produtec/app/web/controller/ProdutoController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 
-import br.com.produtec.app.QProduto;
+import br.com.produtec.app.produto.QProduto;
 import br.com.produtec.app.produto.Produto;
 import br.com.produtec.app.produto.repository.ProdutoRepository;
 
@@ -14,7 +14,7 @@ import br.com.produtec.app.produto.repository.ProdutoRepository;
 public class ProdutoController {
 
 	@Autowired
-	private ProdutoRepository produtoRepository; 
+	private ProdutoRepository produtoRepository;
 
 	public List<Produto> autocomplete(String query) {
 		return produtoRepository.findAll(QProduto.produto.nome.containsIgnoreCase(query), new PageRequest(0, 5))
